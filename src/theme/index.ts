0ec1803a91bfc2fdbbaa3cf52fb2b691e8bb37bc
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2022-12-06 13:17:35 -0300

    Feat: create theme and Home

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+    COLORS: {
+        //PRODUCT
+        PURPLE: '#8284FA',
+        DARK_PURPLE: '#5E60CE',
+        BLUE: '4EA8DE',
+        DARK_BLU: '1E6F9F',
+
+        //BASE
+        GRAY_700: '#0D0D0D',
+        GRAY_600: '#1A1A1A',
+        GRAY_500: '#262626',
+        GRAY_400: '#333333',
+        GRAY_300: '#808080',
+        GRAY_200: '#D9D9D9',
+        GRAY_100: '#F2F2F2',
+
+        //DANGER
+        DANGER: 'E25858',
+    },
+    FONT_FAMILY: {
+        INTER_REGULAR: 'Inter-Regular',
+        INTER_BOLD: 'Inter-Bold',
+    },
+    LINE_HEIGHT: {
+        SIMPLE: 1.4,
+    },
+    FONT_SIZE: {
+        S: 12,
+        M: 14,
+        L: 16,
+    },
+}
