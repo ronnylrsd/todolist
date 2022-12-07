89d0791982c3e09cbcf3f271f3a3a5ea8d7a027f
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2022-12-07 13:35:02 -0300

    Feat: create logo and background

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,17 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Welcome, Gabriel</Text>
-    </View>
+    <>
+      <View style={styles.header}>
+        <Image style={styles.logoImage} source={require("../../assets/rocket.png")} />
+        <Text style={styles.logoTo}>to</Text>
+        <Text style={styles.logoDo}>do</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Welcome, Gabriel</Text>
+      </View>
+    </>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,12 +2,40 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 0.8,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#333333", //GRAY_400
+  },
+  header: {
+    flex: 0.2,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0D0D0D", //GRAY_700
+    flexDirection: "row",
+  },
+  logoImage: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginRight: 10,
+  },
+  logoTo: {
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#4EA8DE",
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  logoDo: {
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#8284FA",
+    fontSize: 30,
+    fontWeight: "bold",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
+    color: "#FFFFFF",
   },
 });
